6715099a2942a78c9f2fa3bf056ec30e542d6050
Nano (Kira AI) <nano@kira.local>
2026-06-06 15:03:13 +0000

    fix: add missing CategoryModel import in adhkar_screen.dart

--- lib/features/adhkar/adhkar_screen.dart
+++ lib/features/adhkar/adhkar_screen.dart
@@ -5,6 +5,7 @@ import '../../core/theme/app_colors.dart';
 import '../../core/theme/app_text_styles.dart';
 import '../../core/constants/app_constants.dart';
 import '../../data/repositories/adhkar_repository.dart';
+import '../../data/models/adhkar_model.dart';
 import '../../shared/widgets/arabesque_bg.dart';
 
 class AdhkarScreen extends ConsumerWidget {
